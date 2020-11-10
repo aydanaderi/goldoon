fe27a750278751d5f4f8aaa134e279489319cdff
ayda@ayda <ayda.f.naderi@gmail.com>
2020-11-10 18:25:22 +0330

    edit

--- Account/models.py
+++ Account/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model) :
-    username =  models.CharField(max_length = 100)
+    username =  models.CharField(max_length = 100,unique = True)
     password = models.CharField(max_length = 100)
     email = models.EmailField(max_length = 500)
     profile = models.ImageField(default = 'pic.jpg')
