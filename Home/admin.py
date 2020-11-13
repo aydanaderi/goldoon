89a0cd1c0935107f34c40aba7b5213bf6614dbb9
ayda@ayda <ayda.f.naderi@gmail.com>
2020-11-13 15:29:47 +0330

    edit admin

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Plant)
+class PlantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'English_name')
+    list_filter = ('name', 'English_name')
+    search_fields = ('name', 'English_name')
