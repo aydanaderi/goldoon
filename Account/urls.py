38b8418a25112a3104a3f90c7569986ed0e065e3
ayda@ayda <ayda.f.naderi@gmail.com>
2020-11-15 19:10:32 +0330

    edit

--- Account/urls.py
+++ Account/urls.py
@@ -3,7 +3,7 @@ from knox import views as knox_views
 from .views import LoginAPI,RegisterAPI,ChangePasswordView
 
 urlpatterns = [
-    path('signup/', RegisterAPI.as_view(), name = 'register'),
+    path('signup', RegisterAPI.as_view(), name = 'register'),
     path('login/', LoginAPI.as_view(), name = 'login'),
     path('logout/', knox_views.LogoutView.as_view(), name = 'logout'),
     path('logoutall/', knox_views.LogoutAllView.as_view(), name = 'logoutall'),
