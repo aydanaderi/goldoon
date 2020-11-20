acee960aa4c88441e0fa83962d2bf09f1228dfce
ayda@ayda <ayda.f.naderi@gmail.com>
2020-11-20 16:24:34 +0330

    edit

--- goldoon/settings.py
+++ goldoon/settings.py
@@ -88,8 +88,8 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
         'knox.auth.TokenAuthentication',
     ]
 }
