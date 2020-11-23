2d7c7240aba0308270ae4fc96f4622c956f68fa4
ayda@ayda <ayda.f.naderi@gmail.com>
2020-11-23 18:49:14 +0330

    edit

--- Account/urls.py
+++ Account/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('login/', views.LoginAPI.as_view(), name = 'login'),
     path('logout/', knox_views.LogoutView.as_view(), name = 'logout'),
     path('change_password/', views.ChangePasswordView.as_view(), name = 'change-password'),
-    path('reset/', views.ResetPasswodView, name = 'reset'),
-    path('<int:username_id>/reset/confirm/', views.ConfirmResetPasswodView , name = 'Reset_password')
+    path('reset/', views.ResetPasswodView, name = 'Reset_Password'),
+    path('<int:username_id>/reset/confirm/', views.ConfirmResetPasswodView , name = 'confirm_Reset_password')
 ]
